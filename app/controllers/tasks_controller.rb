ab92b52248aa77fb2336960c950e9fca98ce159f
naoki.kato <20120215.cloudysky@gmail.com>
2021-01-09 14:42:58 +0000

    kadai toukoukinou

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,18 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :set_task, only: [:show,  :update]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy,]
   
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
+      @user = User.find(params[:id])
+      @tasks = @user.tasks.order(id: :desc).page(params[:page])
   end
 
   def new
@@ -13,18 +20,19 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = "Task が正常に登録されました"
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = "Task が登録されませんでした"
-      render :new
+      render "tasks/index"
     end
   end
 
   def edit
+      @task = Task.find_by(id: params[:id])
   end
 
   def update
@@ -39,9 +47,8 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    
     flash[:success] = "Taskは正常に削除されました"
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
  private
@@ -55,4 +62,10 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
 
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+    redirect_to root_url
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @tasks = @user.tasks.order(id: :desc).page(params[:page])
   end
 
   def new

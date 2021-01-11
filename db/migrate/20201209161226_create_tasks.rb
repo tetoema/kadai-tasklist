a321d05d2dd7f08cd3a1da8d2ef94295010ccbe0
naoki.kato <20120215.cloudysky@gmail.com>
2021-01-11 14:04:04 +0000

    20210111

--- db/migrate/20201209161226_create_tasks.rb
+++ db/migrate/20201209161226_create_tasks.rb
@@ -2,8 +2,6 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :content
-      t.string :status #カラム追加(status)
-      t.references :user, foreign_key: true #カラム追加（user_id）
 
       t.timestamps
     end
